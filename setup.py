979967a13e1ebec8bcf94f87a1dbfc49992e179a
veritaem <dcharris776@gmail.com>
2019-06-03 13:46:24 -0400

    this is a message

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+REQUIRED = [
+        "numpy",
+        "pandas",
+        "sklearn",
+        ]
+
+
+with open("README.md", "r") as fh:
+LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+        name = 
+        version = "0.0.1"
+        author = 
+        description
+        long_description = 
+        long_description_coontent_type = 
+        url = 
+        packages = setuptools.find_packages()
+        python_requires = >= "3.7"
+        
+        
+        
+        )
